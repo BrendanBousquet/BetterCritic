98c1747189c385f157511535ea4d157497d56013
Jeremy Daigneau <jdaigneau5@gmail.com>
2017-03-07 15:41:47 -0500

    Django framwork setup

--- critic/views.py
+++ critic/views.py
@@ -3,5 +3,9 @@ from django.http import HttpResponse
 from django.template import loader
 
 def index(request):
-    template = loader.get_template('critic/index.html')
+    template = loader.get_template('critic/test.html')
+    return HttpResponse(template.render(request))
+
+def test(request):
+    template = loader.get_template('critic/test.html')
     return HttpResponse(template.render(request))
