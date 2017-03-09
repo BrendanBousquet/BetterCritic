7b58cd1ca8ca994461dd8883e326e5ed25d3e2a7
Brandon Route <bar13195@gmail.com>
2017-03-09 16:43:06 -0500

    database, population, database applied to home

--- critic/views.py
+++ critic/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader
+from django.template import RequestContext, loader
+from critic.models import Game
 
 def index(request):
+    games = Game.objects.all()
+    context = RequestContext(request, {'list': games})
     template = loader.get_template('critic/test.html')
-    return HttpResponse(template.render(request))
+    return HttpResponse(template.render(context))
 
 def test(request):
     template = loader.get_template('critic/test.html')
